3d2fa12ff72559f7276d2628e4abea0f15cebbd4
William Cahill <drewlikesramen@gmail.com>
2022-03-26 19:51:07 -0400

    Fixing some iteration code

--- src/physics/collision.rs
+++ src/physics/collision.rs
@@ -1,5 +1,5 @@
 use bevy::{utils::HashMap};
-use bevy::math::{Vec3, UVec3};
+use bevy::math::{Vec3, UVec3, UVec2, IVec3};
 
 /// All of the terrain in a [`World`] at a given time as a resource.
 pub struct Terrain {
@@ -54,13 +54,46 @@ impl Terrain {
     /// Iterates over chunks within a range.
     /// Both min and max are inclusive.
     /// If max values are < min values (max.x < min.x || max.y < min.y || max.z < min.z), iterator will be empty.
-    pub fn iter_chunks<'terrain>(&'terrain self, min: ChunkCoords, max: ChunkCoords) -> impl Iterator<Item=ChunkRef<'terrain>> {
+    pub fn iter_chunks<'terrain>(
+        &'terrain self,
+        min: ChunkCoords,
+        max: ChunkCoords
+    ) -> impl Iterator<Item=ChunkRef<'terrain>> {
         ChunkIter {
             terrain: self,
             min,
             max,
             pos: min
         }
+    }
+
+    /// Iterates over all terrain pieces within global range specified
+    pub fn iter_pieces(&self, min: Coords, max: Coords) -> impl Iterator<Item=TerrainPieceRef> + '_ {
+        let chunk_min = self.to_chunk_coords(min);
+        let chunk_max = self.to_chunk_coords(max);
+        self.iter_chunks(chunk_min, chunk_max)
+            .flat_map(move |chunk| {
+                let (inter_min, inter_max) = chunk.intersect(min, max);
+                let local_min = UVec3::new(
+                    (inter_min.x - chunk.position.x) as u32,
+                    (inter_min.y - chunk.position.y) as u32,
+                    (inter_min.z - chunk.position.z) as u32
+                );
+                let local_max = UVec3::new(
+                    (inter_max.x - chunk.position.x) as u32,
+                    (inter_max.y - chunk.position.y) as u32,
+                    (inter_max.z - chunk.position.z) as u32
+                );
+                println!("min: {:?}, max: {:?}", min, max);
+                println!("Inter min: {:?}, inter max: {:?}", inter_min, inter_max);
+                println!("Chunk pos: {:?}!!!!!!!!!!!!!!", chunk.position);
+                println!("Local min: {:?}, local max: {:?}", local_min, local_max);
+                chunk.iter_pieces(local_min, local_max)
+            })
+            .filter(|piece| match piece.piece {
+                TerrainPiece::Empty => false,
+                _ => true
+            })
     }
 
     /// Gets reference to chunk at specified coordinates.
@@ -92,6 +125,18 @@ impl Terrain {
         let idx = self.chunk_size.x * (self.chunk_size.y*chunk_z + chunk_y) + chunk_x;
         (chunk_coords, idx as usize)
     }
+
+    /// Converts global coords to chunk coords
+    fn to_chunk_coords(&self, coords: Coords) -> ChunkCoords {
+        let chunk_width = self.chunk_size.x as i32;
+        let chunk_height = self.chunk_size.y as i32;
+        let chunk_depth = self.chunk_size.z as i32;
+        ChunkCoords {
+            x: div(coords.x, chunk_width),
+            y: div(coords.y, chunk_height),
+            z: div(coords.z, chunk_depth)
+        }
+    }
 }
 
 /// One piece of terrain
@@ -104,9 +149,9 @@ pub enum TerrainPiece {
 
 /// Reference to terrain piece with context
 #[derive(Debug,Copy, Clone, Eq, PartialEq)]
-pub struct TerrainPieceRef {
+pub struct TerrainPieceRef<'terrain> {
     /// Terrain piece referenced
-    pub piece: TerrainPiece,
+    pub piece: &'terrain TerrainPiece,
 
     /// Global coordinates of the terrain piece being iterated over
     pub coords: Coords
@@ -120,23 +165,18 @@ pub struct ChunkIter<'terrain> {
 }
 
 impl<'terrain> ChunkIter<'terrain> {
-    fn next_pos(pos: &ChunkCoords, min: &ChunkCoords, max: &ChunkCoords) -> Option<ChunkCoords> {
+    fn next_pos(pos: &ChunkCoords, min: &ChunkCoords, max: &ChunkCoords) -> ChunkCoords {
         let mut pos = *pos;
         pos.x += 1;
-        if pos.x > max.x {
+        if pos.x >= max.x {
             pos.x = min.x;
             pos.y += 1;
-            if pos.y > max.y {
+            if pos.y >= max.y {
                 pos.y = min.y;
                 pos.z += 1;
             }
         }
-        if pos.z <= max.z {
-            Some(pos)
-        }
-        else {
-            None
-        }
+        pos
     }
 }
 
@@ -144,29 +184,33 @@ impl<'terrain> Iterator for ChunkIter<'terrain> {
     type Item = ChunkRef<'terrain>;
     fn next(&mut self) -> Option<Self::Item> {
 
-        // Quits early if searching is done
-        if self.pos.z > self.max.z {
+        // Quits early if past bounds
+        if self.pos.z >= self.max.z {
             return None;
         }
 
         // Gets chunk at current location and updates position
         let (mut pos, min, max) = (self.pos, self.min, self.max);
+        println!("Trying: {:?}", pos);
         let mut chunk = self.terrain.get_chunk(pos);
         while chunk.is_none() {
-            pos = Self::next_pos(&pos, &min, &max)?;
+            pos = Self::next_pos(&pos, &min, &max);
+            if pos.z >= self.max.z { return None; }
+            println!("Trying inner: {:?}", pos);
             chunk = self.terrain.get_chunk(pos);
+            println!("None inner: {}", chunk.is_none());
         }
-        self.pos = Self::next_pos(&self.pos, &self.min, &self.max)?;
+        self.pos = Self::next_pos(&pos, &min, &max);
 
-        // Unbounds reference
+        // Unbounds reference and returns next chunk
         unsafe {
-            let result_ptr = chunk? as *const Chunk;
+            let result_ptr = chunk.unwrap() as *const Chunk;
             Some(ChunkRef {
                 chunk: &*result_ptr,
                 position: Coords::new(
-                    self.pos.x * self.terrain.chunk_size.x as i32,
-                    self.pos.y * self.terrain.chunk_size.y as i32,
-                    self.pos.z * self.terrain.chunk_size.z as i32
+                    pos.x * self.terrain.chunk_size.x as i32,
+                    pos.y * self.terrain.chunk_size.y as i32,
+                    pos.z * self.terrain.chunk_size.z as i32
                 ),
                 size: self.terrain.chunk_size
             })
@@ -177,10 +221,106 @@ impl<'terrain> Iterator for ChunkIter<'terrain> {
 /// Chunk of terrain pieces
 pub struct Chunk(Vec<TerrainPiece>);
 
+#[derive(Copy, Clone)]
 pub struct ChunkRef<'terrain> {
     pub chunk: &'terrain Chunk,
     pub position: Coords,
     pub size: UVec3
+}
+
+impl<'terrain> ChunkRef<'terrain> {
+    fn intersect(&self, min: Coords, max: Coords) -> (Coords, Coords) {
+        let this_min = self.position;
+        let this_max = Coords::new(
+            self.position.x + self.size.x as i32,
+            self.position.y + self.size.y as i32,
+            self.position.z + self.size.z as i32
+        );
+        let result_min = this_min.max(min);
+        let mut result_max = this_max.min(max);
+        if result_max.x < result_min.x {
+            result_max.x = 0;
+        }
+        if result_max.y < result_min.y {
+            result_max.y = 0;
+        }
+        if result_max.z < result_min.z {
+            result_max.z = 0;
+        }
+        (
+            result_min,
+            result_max
+        )
+    }
+}
+
+pub struct ChunkRefIter<'terrain> {
+    chunk: ChunkRef<'terrain>,
+    min: UVec3,
+    max: UVec3,
+    pos: UVec3
+}
+
+impl<'terrain> ChunkRef<'terrain> {
+    pub fn iter_pieces(self, min: UVec3, max: UVec3) -> ChunkRefIter<'terrain> {
+        ChunkRefIter {
+            chunk: self,
+            min,
+            max,
+            pos: min
+        }
+    }
+}
+
+impl<'terrain> ChunkRefIter<'terrain> {
+    pub fn new(chunk: ChunkRef<'terrain>, min: UVec3, max: UVec3) -> Self {
+        Self {
+            chunk,
+            min,
+            max,
+            pos: min
+        }
+    }
+}
+
+impl<'terrain> Iterator for ChunkRefIter<'terrain> {
+    type Item = TerrainPieceRef<'terrain>;
+    fn next(&mut self) -> Option<Self::Item> {
+
+        // Quits early if at end
+        if self.pos.z >= self.max.z {
+            return None;
+        }
+
+        // Gets current piece
+        let min = &self.min;
+        let chunk = &self.chunk;
+        let size = &chunk.size;
+        let idx = self.pos.z * (size.y + size.x) + self.pos.y * size.x + self.pos.x;
+        let piece = &self.chunk.chunk.0[idx as usize];
+        println!("Piece idx: {:?}, pos: {:?}, piece: {:?}", idx, self.pos, piece);
+
+        // Advances position
+        self.pos.x += 1;
+        if self.pos.x >= self.max.x {
+            self.pos.x = min.x;
+            self.pos.y += 1;
+            if self.pos.y >= self.max.y {
+                self.pos.y = min.y;
+                self.pos.z += 1;
+            }
+        }
+
+        // Done
+        Some(TerrainPieceRef {
+            piece,
+            coords: Coords::new(
+                chunk.position.x + self.pos.x as i32,
+                chunk.position.y + self.pos.y as i32,
+                chunk.position.z + self.pos.z as i32
+            )
+        })
+    }
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
@@ -202,6 +342,20 @@ pub struct Coords {
 }
 impl Coords {
     pub fn new(x: i32, y: i32, z: i32) -> Self { Self { x, y, z} }
+    pub fn min(self, other: Self) -> Self {
+        Self::new(
+            self.x.min(other.x),
+            self.y.min(other.y),
+            self.z.min(other.z)
+        )
+    }
+    pub fn max(self, other: Self) -> Self {
+        Self::new(
+            self.x.max(other.x),
+            self.y.max(other.y),
+            self.z.max(other.z)
+        )
+    }
     pub fn min_max(self, other: Self) -> (Self, Self) {
         let (min_x, max_x) = if self.x < other.x { (self.x, other.x) } else { (other.x, self.x) };
         let (min_y, max_y) = if self.y < other.y { (self.y, other.y) } else { (other.y, self.y) };
@@ -316,26 +470,42 @@ fn test_chunk_iter() {
         Vec3::new(32.0, 32.0, 32.0),
         UVec3::new(16, 16, 16)
     );
-    let piece = terrain.get_or_create_mut(Coords::new(0, 0, 0));
-    *piece = TerrainPiece::Cuboid;
-    let piece = terrain.get_or_create_mut(Coords::new(-1, 0, 0));
-    *piece = TerrainPiece::Slope;
-    let piece = terrain.get_or_create_mut(Coords::new(0, 0, -1));
-    *piece = TerrainPiece::Cuboid;
+    terrain.get_or_create_mut(Coords::new(0, 0, 0));
+    terrain.get_or_create_mut(Coords::new(-1, 0, 1));
+    terrain.get_or_create_mut(Coords::new(0, 0, -1));
 
     let actual: Vec<ChunkInfo> = terrain.iter_chunks(
-        ChunkCoords::new(-1, 0, -1),
-        ChunkCoords::new(0, 0, 0)
+        ChunkCoords::new(-2, 0, -1),
+        ChunkCoords::new(1, 1, 2)
     )
     .map(|chunk| ChunkInfo {
         pos: chunk.position,
         size: chunk.size
     })
     .collect();
+    
     let expected = vec![
         ChunkInfo { pos: Coords { x: 0, y: 0, z: -16 }, size: UVec3::new(16, 16, 16) },
         ChunkInfo { pos: Coords { x: -16, y: 0, z: 0 }, size: UVec3::new(16, 16, 16) },
         ChunkInfo { pos: Coords { x: 0, y: 0, z: 0 }, size: UVec3::new(16, 16, 16) }
     ];
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn test_selection() {
+    let mut terrain = Terrain::new(
+        Vec3::new(32.0, 32.0, 32.0),
+        UVec3::new(16, 16, 16)
+    );
+    let piece = terrain.get_or_create_mut(Coords::new(5, 5, 5));
+    *piece = TerrainPiece::Cuboid;
+    
+    let actual: Vec<TerrainPiece> = terrain
+        .iter_pieces(Coords::new(4, 4, 4), Coords::new(6, 6, 6))
+        .map(|piece| *piece.piece)
+        .collect();
+    
+    let expected = vec![TerrainPiece::Cuboid];
     assert_eq!(expected, actual);
 }
